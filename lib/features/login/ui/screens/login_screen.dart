2ceac6a54c8e9cab8687e0cbe284eeb077920a84
soukiie <elsayed.saad2014@feps.edu.eg>
2024-05-25 05:10:31 +0300

    finish login page logic and connected with firebase

--- lib/features/login/ui/screens/login_screen.dart
+++ lib/features/login/ui/screens/login_screen.dart
@@ -2,10 +2,13 @@ import 'package:expenses_tracker/core/helpers/spacing.dart';
 import 'package:expenses_tracker/core/theming/fonts.dart';
 import 'package:expenses_tracker/core/theming/my_colors.dart';
 import 'package:expenses_tracker/core/widgets/my_button.dart';
-import 'package:expenses_tracker/core/widgets/my_text_form_field.dart';
+import 'package:expenses_tracker/features/login/logic/cubit/login_cubit.dart';
+import 'package:expenses_tracker/features/login/ui/widgets/build_bloc_listener.dart';
 import 'package:expenses_tracker/features/login/ui/widgets/dont_have_an_account.dart';
+import 'package:expenses_tracker/features/login/ui/widgets/email_and_password.dart';
 import 'package:expenses_tracker/features/login/ui/widgets/terms_and_conditions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,6 +22,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: MyColors.blackColor,
       body: SafeArea(
         child: Padding(
@@ -27,37 +31,8 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                verticalSpace(150),
-                Text(
-                  'Welcome Back!',
-                  style: FontHelper.font28SemiBoldWhite,
-                ),
-                verticalSpace(30),
-                Text(
-                  'Email',
-                  style: FontHelper.font18BoldWhite,
-                ),
-                verticalSpace(10),
-                const MyTextFormField(
-                  hintText: 'Enter your email',
-                  isObscure: false,
-                ),
-                verticalSpace(20),
-                Text('Password', style: FontHelper.font18BoldWhite),
-                verticalSpace(10),
-                MyTextFormField(
-                  isObscure: isObscure,
-                  suffixIcon: IconButton(
-                      icon: isObscure
-                          ? const Icon(Icons.visibility)
-                          : const Icon(Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          isObscure = !isObscure;
-                        });
-                      }),
-                  hintText: 'Enter your password',
-                ),
+                verticalSpace(140),
+                const EmailAndPassword(),
                 verticalSpace(10),
                 Align(
                   alignment: Alignment.centerRight,
@@ -72,18 +47,27 @@ class _LoginScreenState extends State<LoginScreen> {
                     stops: [0.0, 1.0],
                     colors: [MyColors.orangeColor, MyColors.yellowColor],
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    validateThenLogin();
+                  },
                   child: Text('Login', style: FontHelper.font18BoldWhite),
                 ),
                 verticalSpace(20),
                 const TermsAndConditions(),
                 verticalSpace(10),
                 const DontHaveAnAccount(),
+                const BuildBlocListener(),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  validateThenLogin() {
+    if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+      context.read<LoginCubit>().login();
+    }
   }
 }
---
+++ lib/features/login/ui/widgets/build_bloc_listener.dart
@@ -0,0 +1,37 @@
+import 'package:expenses_tracker/core/routing/routes.dart';
+import 'package:expenses_tracker/features/login/logic/cubit/login_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BuildBlocListener extends StatelessWidget {
+  const BuildBlocListener({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginCubit, LoginState>(
+        listenWhen: (previous, current) =>
+            current is LoginSuccessState ||
+            current is LoginLoadingState ||
+            current is LoginErrorState,
+        listener: (context, state) {
+          if (state is LoginLoadingState) {
+            showDialog(
+                context: context,
+                builder: (context) => const Center(
+                      child: CircularProgressIndicator(),
+                    ));
+          } else if (state is LoginErrorState) {
+            Navigator.pop(context);
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Error Loging In'),
+              ),
+            );
+          } else {
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, Routes.home);
+          }
+        },
+        child: const SizedBox.shrink());
+  }
+}
